94270ab8ef5e9c938ad2520f6c1be26398b380a7
Michael Neumann <mneumann@ntecs.de>
2015-04-10 11:27:18 +0200

    Update to rust 1.0-beta

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
-#![feature(core, os, unsafe_destructor, io, io_ext)]
+extern crate libc;
+#[macro_use]
+extern crate log;
 
 use std::fs::File;
-use std::os::{MemoryMap, MapOption};
 use std::os::unix::io::AsRawFd;
 use std::mem;
 use std::slice;
 use std::marker;
-use std::num::Int;
 use std::io::{Result, Error, ErrorKind};
+use std::ptr;
+use std::convert::AsRef;
+
+use libc::{size_t, c_void, c_int, MAP_FAILED};
 
 /**
  * Maps a file into memory and represents this memory
@@ -17,18 +21,21 @@ pub struct MmapSlice<T:Sized> {
     /// Underlying file
     file: File,
 
-    /// The mmap handle
-    mmap: MemoryMap,
+    /// Pointer to the memory mapped region
+    data: *mut u8,
+
+    /// Number of bytes this map applies to
+    len: size_t,
 
     /// Number of elements of type `T`
     nelems: usize,
 
-    ///
+    /// Marker to keep the type T in the signature
     marker: marker::PhantomData<T>,
 }
 
 fn other_io_error(description: &'static str) -> Error {
-    Error::new(ErrorKind::Other, description, None)
+    Error::new(ErrorKind::Other, description)
 }
 
 impl<T:Sized> MmapSlice<T> {
@@ -42,19 +49,27 @@ impl<T:Sized> MmapSlice<T> {
                 if map_len as u64 > meta.len() {
                     return Err(other_io_error("File too small to mmap"))
                 }
-                let opts = [MapOption::MapReadable,
-                            MapOption::MapFd(file.as_raw_fd())];
-                let map = try!(MemoryMap::new(map_len, &opts).map_err(|_| other_io_error("Mmap error")));
-                if map.len() > 0 && map.len() >= map_len {
+
+                let len: size_t = map_len as size_t;
+                let addr: *mut c_void = ptr::null_mut();
+                let prot: c_int = libc::PROT_READ;
+                let flags: c_int = libc::MAP_PRIVATE | libc::MAP_FILE;
+                let offset: libc::off_t = 0;
+                let fd: c_int = file.as_raw_fd();
+
+                let r = unsafe {
+                    libc::mmap(addr, len, prot, flags, fd, offset)
+                };
+		if r == libc::MAP_FAILED {
+                    Err(other_io_error("Mmap error"))
+		} else {
                     Ok(MmapSlice {
                         file:   file,
-                        mmap:   map,
+                        data:   r as *mut u8,
+                        len:    len,
                         nelems: nelems,
                         marker: marker::PhantomData
                     })
-                }
-                else {
-                    Err(other_io_error("Mmaped region too small"))
                 }
             }
             None => {
@@ -64,19 +79,23 @@ impl<T:Sized> MmapSlice<T> {
     }
 }
 
-impl<T:Sized> AsSlice<T> for MmapSlice<T> {
-    fn as_slice<'a>(&'a self) -> &'a [T] {
+impl<T:Sized> AsRef<[T]> for MmapSlice<T> {
+    fn as_ref(&self) -> &[T] {
          unsafe {
-             slice::from_raw_parts(self.mmap.data() as *const T, self.nelems)
+             slice::from_raw_parts(self.data as *const T, self.nelems)
          }
     }
 }
 
-// FIXME: Do we really need our custom Drop?
-#[unsafe_destructor]
 impl<T:Sized> Drop for MmapSlice<T> {
     fn drop(&mut self) {
-        drop(&mut self.mmap);
+        unsafe {
+            match libc::munmap(self.data as *mut c_void, self.len) {
+                0 => (),
+                r => error!("Unexpected result {}", r)
+            }
+        }
+
         drop(&mut self.file);
     }
 }
